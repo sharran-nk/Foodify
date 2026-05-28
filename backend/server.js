d14affa4f153926dfd9e770b80eaf7d83bcb8b71
nandauforians <sharran.nanda06@gmail.com>
2026-05-28 14:28:53 +0530

    render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express  from "express"
+import express from "express"
 import cors from 'cors'
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
@@ -22,12 +22,12 @@ connectDB()
 // api endpoints
 app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images", express.static('uploads'))
 app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res) => {
-    res.send("API Working")
-  });
+  res.send("API Working")
+});
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
--- upload.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git init
-git remote add origin https://github.com/Sharran06/food-del-application.git
-git fetch origin
-git branch -M main
-git merge origin/main --allow-unrelated-histories
-git add .
-git commit -m "Complete upload of project files"
-git push -u origin main
